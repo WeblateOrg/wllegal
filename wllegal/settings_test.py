573dc03fbe208270d23fd99c8998c18aa75e59f2
Michal Čihař <michal@cihar.com>
2021-08-30 08:16:40 +0200

    Fix settings modification

--- wllegal/settings_test.py
+++ wllegal/settings_test.py
@@ -21,6 +21,6 @@ import os.path
 
 from weblate.settings_test import *
 
-INSTALLED_APPS.insert(0, "wllegal")
+INSTALLED_APPS = ("wllegal",) + INSTALLED_APPS
 
 LOCALE_PATHS = [os.path.join(os.path.dirname(__file__), "locale")]
