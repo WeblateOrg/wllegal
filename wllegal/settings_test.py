937427c2c314e06808a49047735ed381b2fc0490
Michal Čihař <michal@cihar.com>
2025-11-20 15:42:08 +0100

    fix: avoid Weblate locale filters

--- wllegal/settings_test.py
+++ wllegal/settings_test.py
@@ -9,3 +9,4 @@ from weblate.settings_test import *  # noqa: F403
 INSTALLED_APPS = ["wllegal", *list(INSTALLED_APPS)]  # noqa: F405
 
 LOCALE_PATHS = [os.path.join(os.path.dirname(__file__), "locale")]
+LOCALE_FILTER_FILES = False
